2e8a82b9d5df996cdb3e6c25e01a15cffc4dc05f
lakrounehamza <lakrounehamza10@gmail.com>
2026-01-22 14:18:51 +0100

    feat(app): update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { MenuCatalogComponent } from './menu-catalog/menu-catalog.component';
+
+export const routes: Routes = [
+	{ path: '', component: MenuCatalogComponent },
+];
