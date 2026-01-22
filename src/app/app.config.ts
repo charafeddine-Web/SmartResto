15f8e983e9af9cc073887df5c29b5b04f9f2f4dc
lakrounehamza <lakrounehamza10@gmail.com>
2026-01-22 14:18:51 +0100

    fix(app): update app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { productReducer } from './menu-catalog/product.reducer';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideStore({ productState: productReducer }),
+    provideStoreDevtools({ maxAge: 25, logOnly: false })
   ]
 };
